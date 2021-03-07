1462e9741e7fe8bffd8154c6c609c5a91dd9db66
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-07 14:05:08 +0530

    Create Common.js

---
+++ Js/Model/Common.js
@@ -0,0 +1,12 @@
+class Item {
+    constructor(name, value) {
+        this.Name = name;
+        this.Value = value;
+      }
+ }
+class RecoPost {
+  constructor(title,  link) {
+    this.Title = title;
+    this.Link = link;
+  }
+}
