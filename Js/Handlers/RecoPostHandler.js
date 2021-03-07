28f4037774b40702b631b92eac8aefe90749a4ea
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-07 16:08:09 +0530

    Update RecoPostHandler.js

--- Js/Handlers/RecoPostHandler.js
+++ Js/Handlers/RecoPostHandler.js
@@ -25,7 +25,7 @@ var BindInsideTheme = (recoList) => {
             document.getElementById("recommended_label").classList.add('justify-content-center');
             document.getElementById("reco_post").classList.add('m-top-30');
             console.log(recoList);
-            var rcl = "<ul class='m-left-50'>";
+            var rcl = "<ul style='margin-left:20%;'>";
             for (let i=0; i<recoList.length; i++) {
                  rcl += "<a href=" + recoList[i].Link  + " style='color: #00ff00;' class='m-16'><li>" + recoList[i].Title + "</li></a>";
              }
