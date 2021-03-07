c406fe6a18d12e50d82b70039572dfea8e4c010e
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-07 16:11:58 +0530

    Update RecoPostHandler.js

--- Js/Handlers/RecoPostHandler.js
+++ Js/Handlers/RecoPostHandler.js
@@ -1,16 +1,5 @@
-// var recommendedList = {
-//             "What is a Dictionary in C# testing": "https://cseatoz.blogspot.com/2020/10/c-dictionary.html",
-//             "C# Dictionary Property": "https://cseatoz.blogspot.com/2020/10/c-dictionary-properties.html",
-//           	"C# Dictionary Add() Method":"https://cseatoz.blogspot.com/2020/10/c-dictionary-function.html"
-//         };
-// var rcl = "<ul style='margin:50px 25px 25px 25px; '>";
-// for (let k in recommendedList) {
-//      rcl += "<a href=" + recommendedList[k] + " style='color: #00ff00;'><li>" + k + "</li></a>";
-//  }
 
-//  rcl += "</ul>";
-// document.getElementById("recommended").innerHTML = rcl;
-var home="https://cseatoz.blogspot.com"
+var home="https://cseatoz.blogspot.com";
 
 var GetRecoPostByDesc = () =>{
             let recoList=[];
@@ -25,7 +14,7 @@ var BindInsideTheme = (recoList) => {
             document.getElementById("recommended_label").classList.add('justify-content-center');
             document.getElementById("reco_post").classList.add('m-top-30');
             console.log(recoList);
-            var rcl = "<ul style='margin-left:20%;'>";
+            var rcl = "<ul style='margin-left:15%;'>";
             for (let i=0; i<recoList.length; i++) {
                  rcl += "<a href=" + recoList[i].Link  + " style='color: #00ff00;' class='m-16'><li>" + recoList[i].Title + "</li></a>";
              }
