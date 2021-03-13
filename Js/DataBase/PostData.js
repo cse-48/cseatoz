4f168351e4ededf535aa033070da965b05964799
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-13 15:18:39 +0530

    Create PostData.js

---
+++ Js/DataBase/PostData.js
@@ -0,0 +1,21 @@
+var _postDataDict= {};
+
+var post = new RecoPost("What is a Dictionary in C#","/2020/10/c-dictionary.html");
+post.Labels="c#,dictionary";
+_postDataDict[post.Id]=post;
+
+post=new RecoPost("C# Dictionary Add() Method","/2020/10/c-dictionary-function.html");
+post.Labels="c#,dictionary,method,add";
+_postDataDict[post.Id]=post;
+
+post=new RecoPost("C# Dictionary Property","/2020/10/c-dictionary-properties.html");
+post.Labels="c#,dictionary,property";
+_postDataDict[post.Id]=post;
+
+post=new RecoPost("C# Dictionary iteration method","/2020/10/Iterate-a-dictionar-in-Csharp.html");
+post.Labels="c#,dictionary,iterate,loop,property";
+_postDataDict[post.Id]=post;
+
+post=new RecoPost("What is React and how to include in html project","/2020/08/react.html");
+post.Labels="react";
+_postDataDict[post.Id]=post;
