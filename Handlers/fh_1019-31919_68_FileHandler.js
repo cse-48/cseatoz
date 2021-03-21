1bf0263ae634bb5fa0aef82893262e7e1c9c2ae0
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-21 15:06:21 +0530

    Update fh_1019-31919_68_FileHandler.js

--- Handlers/fh_1019-31919_68_FileHandler.js
+++ Handlers/fh_1019-31919_68_FileHandler.js
@@ -1,9 +1,11 @@
 // JS File Handlers
-
+document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/Model/Common.js'/>");
+document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/DataBase/PostData.js'/>");
+document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
 var DefaultJS = () => {
-  document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/Model/Common.js'/>");
-  document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/DataBase/PostData.js'/>");
-  document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
+//   document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/Model/Common.js'/>");
+//   document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/DataBase/PostData.js'/>");
+//   document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
   
 //   document.writeln("<script  src=''></script>");
 };
