6917e5973cdf876333374532839796cc0af485b4
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-21 15:36:44 +0530

    Update fh_1019-31919_68_FileHandler.js

--- Handlers/fh_1019-31919_68_FileHandler.js
+++ Handlers/fh_1019-31919_68_FileHandler.js
@@ -1,22 +1,22 @@
-// JS File Handlers
+// // JS File Handlers
  
-var home="https://cse-48.github.io/cseatoz";
+// var home="https://cse-48.github.io/cseatoz";
 
-var defaultJsList = ["/Js/Model/Common.js","/Js/DataBase/PostData.js","/MetaData/js_cookie.js"];
+// var defaultJsList = [];
 
-var DefaultJS = () => {
-  defaultJsList.forEach((path) => {
-      var script = document.createElement("script");
-      script.src = home + path;
-      document.head.appendChild(script);
-  });
-};
-DefaultJS();
+// var DefaultJS = () => {
+//   defaultJsList.forEach((path) => {
+//       var script = document.createElement("script");
+//       script.src = home + path;
+//       document.head.appendChild(script);
+//   });
+// };
+// DefaultJS();
 
 
 
-// CSS File Handlers
-var DefaultCSS = () => {
+// // CSS File Handlers
+// var DefaultCSS = () => {
   
-};
-DefaultCSS();
+// };
+// DefaultCSS();
