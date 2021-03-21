6127b8e78c7fae1b0fde11827475b9b10f3b0caa
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-21 15:26:29 +0530

    Update fh_1019-31919_68_FileHandler.js

--- Handlers/fh_1019-31919_68_FileHandler.js
+++ Handlers/fh_1019-31919_68_FileHandler.js
@@ -1,26 +1,20 @@
 // JS File Handlers
-// document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/Model/Common.js'/>");
-// document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/DataBase/PostData.js'/>");
-// document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
+ 
 var home="https://cse-48.github.io/cseatoz";
 
-var defaultJsList = ["/Js/Model/Common.js'","/Js/DataBase/PostData.js","/MetaData/js_cookie.js"];
+var defaultJsList = ["/Js/Model/Common.js","/Js/DataBase/PostData.js","/MetaData/js_cookie.js"];
 
 var DefaultJS = () => {
-//       var script = document.createElement("script");
-//       script.src="https://cse-48.github.io/cseatoz/Js/Model/Common.js"
   defaultJsList.forEach((path) => {
       var script = document.createElement("script");
       script.src = home + path;
       document.head.appendChild(script);
   });
-//   document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/Model/Common.js'/>");
-//   document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/DataBase/PostData.js'/>");
-//   document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
-  
-//   document.writeln("<script  src=''></script>");
 };
 DefaultJS();
+
+
+
 // CSS File Handlers
 var DefaultCSS = () => {
   
