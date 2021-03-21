33e2a71ba6ad2aa36fa02e1b1dab99adfca8dc06
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-21 15:00:41 +0530

    Update fh_1019-31919_68_FileHandler.js

--- Handlers/fh_1019-31919_68_FileHandler.js
+++ Handlers/fh_1019-31919_68_FileHandler.js
@@ -1,9 +1,10 @@
 // JS File Handlers
 
 var DefaultJS = () => {
-  document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
   document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/Model/Common.js'/>");
   document.writeln("<script src='https://cse-48.github.io/cseatoz/Js/DataBase/PostData.js'/>");
+  document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
+  
 //   document.writeln("<script  src=''></script>");
 };
 DefaultJS();
