00faaa1c8a9e4d24e58f368c9f5dd3dff917e423
cse-48 <74768952+cse-48@users.noreply.github.com>
2021-03-21 14:45:02 +0530

    Create fh_1019-31919_68_FileHandler.js

---
+++ Handlers/fh_1019-31919_68_FileHandler.js
@@ -0,0 +1,14 @@
+// JS File Handlers
+
+var DefaultJS = () => {
+  document.writeln("<script src='https://cse-48.github.io/cseatoz/MetaData/js_cookie.js'/>");
+//   document.writeln("<script  src=''></script>");
+//   document.writeln("<script  src=''></script>");
+//   document.writeln("<script  src=''></script>");
+};
+DefaultJS();
+// CSS File Handlers
+var DefaultCSS = () => {
+  
+};
+DefaultCSS();
